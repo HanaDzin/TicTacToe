ab09cfe1fb744429e2aad90fc6cd4eac4bc48f0d
HanaDzin <dzinhana@hotmail.com>
2024-11-13 10:11:15 +0100

    implement Player component & logic

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+
+  //show the player name or an input if we want to edit it
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing)
+    playerName = (
+      <input type="text" value={name} required onChange={handleChange} />
+    );
+
+  function handleEditClick() {
+    //to make sure you are working with the latest state value, pass a function to the state updating function
+    setIsEditing((editing) => !editing);
+    //instead of: setIsEditing(!isEditing)
+  }
+
+  //handles logic when onChange event happens on input field - every time user enters a char
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
